a2927c0fd8c553bd8306b6864a3892221cb4023a
SergBobyr <serg_bobyr@ukr.net>
2023-08-07 20:04:57 +0300

    fix space-x

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = () => {
+    const [launches, setLaunches]=useState([])
+    useEffect(()=>{fetch('https://api.spacexdata.com/v3/launches/').then(resp=>resp.json()).then(resp=> setLaunches(resp))},[])
     return (
-        <div>
-          App
+        <div >
+            {launches.filter(item=>item.launch_year!=='2020').map(launche=>{
+                return(
+                <div key={launche.flight_number}>{launche.mission_name} --- {launche.launch_year}</div>)})}
         </div>
     );
 };
